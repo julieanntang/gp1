1dbaff74bbdef60e8e850120cf956f13f458120c
Alan Mamulski <alanmamulski@gmail.com>
2021-09-08 15:56:39 -0600

    teams and league controllers

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -1,2 +1,46 @@
 class Api::LeaguesController < ApplicationController
+
+  
+  def index
+    leagues = League.all
+    render json: leagues
+  end
+  
+  def show
+    league = League.find(params[:id])
+    render json: league
+  end
+  
+  def create
+    league = League.new(league_params)
+    if league.save
+      render json: league
+    else
+      render json: {errors: league.errors}, status 422
+    end
+  
+    def update
+      league = League.find(params[:id])
+      if league.update(league_params)
+        render json: league
+      else
+        render json: {errors: league.errors}, status 422
+      end
+    end
+  
+    def destroy
+      league = League.find(params[:id])
+      league.destroy
+  
+      render json: league
+    end
+  
+    private
+  
+    def league_params
+      params.require(:league).permit(:name, :description)
+    end
+  
+  
+  
 end
--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,2 +1,50 @@
 class Api::TeamsController < ApplicationController
+
+def index
+  teams = Team.all
+  render json: teams
+end
+
+def show
+  team = League.find(params[:id])
+  render json: team
+end
+
+def create
+  team = Team.new(team_params)
+  if team.save
+    render json: team
+  else
+    render json: {errors: team.errors}, status 422
+  end
+
+  def update
+    team = Team.find(params[:id])
+    if team.update(team_params)
+      render json: team
+    else
+      render json: {errors: team.errors}, status 422
+    end
+  end
+
+  def destroy
+    team = Team.find(params[:id])
+    team.destroy
+
+    render json: team
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :description)
+  end
+
+  def find_team 
+    @team = Team.find(params[:id])
+  end
+
+  def find_league
+    @league = League.find(params[:league_id])
+  end
 end
