1dbdecbc6ea7add17d10490551acd5ca584e75f6
Julie Tang <julieanntang@hotmail.com>
2021-09-09 17:04:35 -0600

    update league done

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Switch, Route } from "react-router-dom";
 import LeagueForm from './components/LeagueForm';
 import Leagues from './components/Leagues';
+import EditLeague from './components/EditLeague';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Leagues} />
         <Route exact path="/leagues/new" component={LeagueForm} />
+        <Route exact path="/leagues/:id/edit" component={EditLeague} />
       </Switch>
     </div>
   );
--- client/src/components/League.js
+++ client/src/components/League.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 
 const League = ({id, name, description, country}) => {
+
+  const history = useHistory();
+
   return (
     <div>
       <h1>{name}</h1>
       <h2>{country}</h2>
       <p>{description}</p>
+      <button onClick={() => history.push(`/leagues/${id}/edit`)}>Update</button>
     </div>
   );
 };
